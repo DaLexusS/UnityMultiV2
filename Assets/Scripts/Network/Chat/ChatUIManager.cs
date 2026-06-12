6f8745c2a2dbbf9cb074fac8d9851cafc64b1360
Mike-Avrius <mike.nidam@gmail.com>
2026-06-12 16:45:53 +0300

    UI logic fix

--- Assets/Scripts/Network/Chat/ChatUIManager.cs
+++ Assets/Scripts/Network/Chat/ChatUIManager.cs
@@ -1,13 +1,18 @@
+using System.Collections;
 using Fusion;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ChatUIManager : MonoBehaviour
 {
    public static ChatUIManager Instance { get; private set; }
+
+   [SerializeField] private TextMeshProUGUI textMessagePrefab;
+   [SerializeField] private Transform content;
    
-   
-   [SerializeField] private TextMeshProUGUI messageField;
+   [SerializeField] private ScrollRect scrollRect;
+  
 
    private void Awake()
    {
@@ -16,6 +21,16 @@ public class ChatUIManager : MonoBehaviour
 
    public void AddMessage(PlayerRef sender, string message)
    {
-      messageField.text = $"{sender}: {message}";
+      TextMeshProUGUI newTextMessage = Instantiate(textMessagePrefab, content);
+      newTextMessage.text = $"{sender} : {message}";
+
+      StartCoroutine(ScrollToBottom());
+   }
+   
+   
+   private IEnumerator ScrollToBottom()
+   {
+      yield return null;
+      scrollRect.verticalNormalizedPosition = 0f;
    }
 }
