7201c6504ee15a8e622b57b7b727b43e65fd2ace
Mike-Avrius <mike.nidam@gmail.com>
2026-06-18 03:50:20 +0300

    Player Spawn in Pos

--- Assets/scriptsNewFolder/FusionSpawnManager.cs
+++ Assets/scriptsNewFolder/FusionSpawnManager.cs
@@ -3,32 +3,39 @@ using UnityEngine;
 
 public class FusionSpawnManager : MonoBehaviour
 {
-    public NetworkObject[] characterPrefabs;
-    public Transform[] spawnPoints;
+    [SerializeField] private NetworkObject[] characterPrefabs;
+    [SerializeField] private Transform[] spawnPoints;
 
     private void Start()
     {
-        int selectedCharacter = PlayerData.SelectedCharacter;
+        NetworkRunner runner = FindFirstObjectByType<NetworkRunner>();
 
-        if (selectedCharacter < 1)
+        if (runner == null)
         {
-            Debug.LogError("No character selected!");
+            Debug.LogError("NetworkRunner not found!");
             return;
         }
 
-        int index = selectedCharacter - 1;
-        NetworkObject selectedPrefab = characterPrefabs[index];
-        Transform selectedSpawnPoint = spawnPoints[index];
+        int selectedCharacter = PlayerData.SelectedCharacter;
 
-        NetworkRunner runner = FindFirstObjectByType<NetworkRunner>();
+        if (selectedCharacter < 1 || selectedCharacter > characterPrefabs.Length)
+        {
+            Debug.LogError("Wrong character index!");
+            return;
+        }
 
-        if (runner != null)
-        {
-            runner.Spawn(selectedPrefab, selectedSpawnPoint.position, Quaternion.identity, runner.LocalPlayer);
-        }
-        else
-        {
-            Instantiate(selectedPrefab.gameObject, selectedSpawnPoint.position, Quaternion.identity);
-        }
+        int characterIndex = selectedCharacter - 1;
+
+        NetworkObject selectedPrefab = characterPrefabs[characterIndex];
+
+        int spawnIndex = (runner.LocalPlayer.PlayerId - 1) % spawnPoints.Length;
+        Transform selectedSpawnPoint = spawnPoints[spawnIndex];
+
+        runner.Spawn(
+            selectedPrefab,
+            selectedSpawnPoint.position,
+            selectedSpawnPoint.rotation,
+            runner.LocalPlayer
+        );
     }
 }
